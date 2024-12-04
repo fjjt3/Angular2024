085241c7f0fe6e4a71459eae069af4ca120bedd0
Francisco Javier Jimenez <>
2024-12-04 08:42:23 +0100

    Add:father/son components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 import { ComponenteEnLineaComponent } from "./componente-en-linea/componente-en-linea.component";
 import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
+import { PadreComponent } from "./padre/padre.component";
 // import { NuevoComponenteComponent } from './nuevo-componente/nuevo-componente.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent, PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
