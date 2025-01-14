ca069135c31b52a3e44be5788edc27bbea8afedf
Francisco Javier Jimenez <>
2025-01-14 14:42:02 +0100

    Add:Head Methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { CabeceroComponent } from "./cabecero/cabecero.component";
 import { FormularioComponent } from "./formulario/formulario.component";
 import { IngresoComponent } from "./ingreso/ingreso.component";
 import { EgresoComponent } from "./egreso/egreso.component";
+import { Ingreso } from './ingreso/ingreso.model';
+import { Egreso } from './egreso/egreso.model';
+import { IngresoService } from './ingreso/ingreso.service';
+import { EgresoService } from './egreso/egreso.service';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +17,38 @@ import { EgresoComponent } from "./egreso/egreso.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-presupuesto';
+  ingresos: Ingreso[]=[];
+  egresos: Egreso[]=[];
+
+  constructor(
+    private ingresoServicio: IngresoService,
+    private egresoService: EgresoService
+  ){
+    this.ingresos = this.ingresoServicio.ingresos;
+    this.egresos = this.egresoService.egresos;
+  }
+
+  getIngresoTotal(){
+    let ingresoTotal = 0;
+    this.ingresos.forEach(ingreso => {
+      ingresoTotal += ingreso.valor;
+    });
+    return ingresoTotal;
+  }
+
+  getEgresoTotal(){
+    let egresoTotal = 0;
+    this.egresos.forEach(egreso => {
+      egresoTotal += egreso.valor;
+    });
+    return egresoTotal;
+  }
+
+  getPorcentajeTotal(){
+    return this.getEgresoTotal()/this.getIngresoTotal();
+  }
+
+  getPresupuestoTotal(){
+    return this.getIngresoTotal() - this.getEgresoTotal();
+  }
 }
