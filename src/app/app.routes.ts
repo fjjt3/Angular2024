0d2f2b595c987f41b07341c223bc37ac1b92cd0d
Francisco Javier Jimenez <>
2025-01-22 12:27:40 +0100

    Add:routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: ListadoProductosComponent},
+    {path:'listado', component: ListadoProductosComponent},
+    {path:'agregar', component: FormularioComponent},
+    {path:'editar:id', component: FormularioComponent} 
+];
